703d03c6c9eb81147624eaa10fc1a60fc9859653
Sagyndyk Mukhambetov <msagyndyk@gmail.com>
2023-05-26 03:28:19 +0600

    Added additional repository for Category

---
+++ src/main/java/kz/vlife/demo/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package kz.vlife.demo.repository;
+
+import kz.vlife.demo.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category> {
+}
